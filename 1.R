347a2f7564a46982de1d105c89a1b56c511f20b3
talahallos <98079252+talahallos@users.noreply.github.com>
2023-04-28 00:06:17 -0400

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,4 +1,4 @@
-## Description: 
+## Description: Comparing RPKM genomic file to fasta gene file
 ## Author: Talah
 ## Initiation date: April 26
 
@@ -16,44 +16,34 @@ install.packages("AnnotationDbi")
 
 # File Names:
 # GSE75897_RiboZero_RPKMs.txt
-# GSE75897_RiboZero_RPKMs.txt
+# GSE75897_RiboZero_RPKMs.gz
 # orf_genomic_1000.fasta
 
+setwd("/Users/talahallos/Desktop/research")
+# list.files() can be used to orient within a file - see what's there
 
 
+
+# AnaCoDa Framework
 
 genome <- initializeGenomeObject(file = "orf_genomic_1000.fasta")
-
-
 # changed to gene.assignment from geneAssignment
 parameter <- initializeParameterObject(genome = genome, sphi = 1, num.mixtures = 1, gene.assignment = rep(1, length(genome)))
-
 model <- initializeModelObject(parameter = parameter, model = "ROC")
-
-
-
 #adjusted sphi, num.mixtures, and gene.assignment which are now random values
 parameter <- initializeParameterObject(genome = genome, sphi = c(0.5, 2), num.mixtures = 2, gene.assignment = sample.int(2, length(genome), replace = T))
-
-
 parameter <- initializeParameterObject(genome = genome, sphi = 1, num.mixtures = 1, gene.assignment = rep(1, length(genome)), init.sepsilon = sepsilon)
-
 model <- initializeModelObject(parameter = parameter, model = "ROC",  fix.observation.noise = TRUE)
+
 
 
 gz_path <- "/Users/talahallos/Desktop/research/GSE75897_RiboZero_RPKMs.gz"
  # Open a connection to the output .gz file using gzfile()
 gz_conn <- gzfile(gz_path, "wb")
 
-
 gz_conn <- gzfile(gz_path, "rb")
 file_contents <- readLines(gz_conn)
 close(gz_conn)
-
-
-con <- gzfile("/Users/talahallos/Desktop/research/GSE75897_RiboZero_RPKMs.gz", "r")
-
-
 
 #.txt file to .gz file!!!!
 text_content <- readLines("GSE75897_RiboZero_RPKMs.txt")
@@ -61,24 +51,24 @@ gzfile_conn <- gzfile("GSE75897_RiboZero_RPKMs.gz", "wb")
 writeLines(text_content,gzfile_conn)
 
 # READING .GZ FILES
-setwd("path/to/folder")
-gzfile_conn <- gzfile("example.txt.gz", "rb")
+gzfile_conn <- gzfile("GSE75897_RiboZero_RPKMs.gz", "rb")
 text_content <- readLines(gzfile_conn)
 close(gzfile_conn)
 cat(text_content, sep = "\n")
 
+# Defining terms to the files & data
 rpkm_genome <- read.delim("GSE75897_RiboZero_RPKMs.gz", header = FALSE, sep = "\t")
-rpkm_names <-rpkm.genome[,1]
+rpkm_names <- rpkm_genome[,1]
+fasta_genome <- head(readLines("/Users/talahallos/Desktop/research/orf_genomic_1000.fasta"), n = 1000)
+f_genome <- initializeGenomeObject(file = "orf_genomic_1000.fasta")
+fasta_names <- getNames(f_genome)
 
 
-head(readLines(filepath), n = 500)
-# assigned filepath <- orf_genomic_1000.fasta file!
-# assigned filecontents <- head(readLines(filepath), n = 500)
-cat(filecontents, sep = "\n")
 
-# SHORTCUTS TO THE TWO FILES:
-# txt.genome = read.delim("GSE75897_RiboZero_RPKMs.gz", header = TRUE, sep = "\t")
-# fasta.genome = head(readLines(filepath), n = 1000)
+
+
+
+
 
 txtgenome <- readLines("GSE75897_RiboZero_RPKMs.gz")
 indices <- grep("^Y", txtgenome)
